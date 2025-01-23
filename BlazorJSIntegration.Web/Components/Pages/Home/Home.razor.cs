b3d90f52c15116a7452bcf19521c7598bb7d1a6d
Nikita Epishev <nikita.epishev@saritasa.com>
2025-01-23 14:39:31 +0300

    Add project files.

---
+++ BlazorJSIntegration.Web/Components/Pages/Home/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorJSIntegration.Web.Components.Pages.Home;
+
+/// <summary>
+/// Home page.
+/// </summary>
+public partial class Home
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
